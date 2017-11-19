63438cf4076ea7b9bfa3192cd04988ede800382f
SSE4 <tomskside@gmail.com>
2017-11-19 19:29:03 +0700

    - update test package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,19 +1,25 @@
-from conans import ConanFile, CMake
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake, tools, RunEnvironment
 import os
 
-class ExpatTestConan(ConanFile):
+
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
-    def imports(self):
-        self.copy("*.dll",    dst="bin", src="bin")
-        self.copy("*.dylib*", dst="lib", src="lib")
-        self.copy("*.so*",    src="lib", dst="lib")
-
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        cmake.configure()
         cmake.build()
 
     def test(self):
-        self.run(os.path.join("bin", "example"))
+        with tools.environment_append(RunEnvironment(self).vars):
+            bin_path = os.path.join("bin", "example")
+            if self.settings.os == "Windows":
+                self.run(bin_path)
+            elif self.settings.os == "Macos":
+                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
+            else:
+                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
